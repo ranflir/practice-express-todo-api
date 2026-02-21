a1278135fe64b6bb882114922c5a19f3d4a244f1
KIMHYUNJE <ranflir.sc@gmail.com>
2026-02-21 16:04:31 +0900

    queryString

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import tasks from './data/mock.js';
+
+const app = express();
+
+app.get('/tasks', (req, res) => {
+  // 쿼리 파라미터
+  /**
+   * sort:'oldest'인 경우 오래된 테스크 기준, 나머지 겨우 새로운 테스크 기준
+   * count:테스크 개수
+   */
+
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  const compareFn =
+    sort === 'oldest'
+      ? (a, b) => a.createdAt - b.createdAt
+      : (a, b) => b.createdAt - a.createdAt;
+
+  let newTasks = tasks.sort(compareFn);
+
+  if (count) {
+    newTasks = newTasks.slice(0, count);
+  }
+  res.send(newTasks);
+});
+
+app.listen(3000, () => console.log('Server Started!'));
