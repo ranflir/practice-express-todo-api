fefbeadb4dd6fe0b03af6ef533fd6b4e1d32e551
KIMHYUNJE <ranflir.sc@gmail.com>
2026-02-23 18:05:57 +0900

    schema

---
+++ models/Task.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    isComplete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Task = mongoose.model('Task', TaskSchema);
+
+export default Task;
